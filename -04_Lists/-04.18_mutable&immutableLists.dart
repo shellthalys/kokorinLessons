232670b82f70cd948ddfcf55290b3b1db17b7b04
“Dmitry” <shelllogist@gmail.com>
2023-03-25 02:04:54 +0200

    first commit

---
+++ -04_Lists/-04.18_mutable&immutableLists.dart
@@ -0,0 +1,10 @@
+//Create List
+void main() {
+  var fruits = ['apple', 'pineapple', 'watermelon'];
+  print(fruits);
+  fruits = [];
+  print(fruits);
+  fruits = ['orange', 'pear'];
+  print(fruits);
+}
+
