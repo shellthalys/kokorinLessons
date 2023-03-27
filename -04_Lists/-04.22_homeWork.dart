2189e80bdc54589345b7890e27d84b51c20b3ab9
“Dmitry” <shelllogist@gmail.com>
2023-03-28 00:15:49 +0300

    [list] выполнение -04.22 Д.З.

--- -04_Lists/-04.22_homeWork.dart
+++ -04_Lists/-04.22_homeWork.dart
@@ -1,5 +1,15 @@
 //Create List
 void main() {
+final list = [60, 999, 14, "dart1", 45, 95, "dart", 1];
+var value1 = 'dart';
+var value2 = 15;
 
+print(list.contains(value1));
+print(list.contains(value2));
 }
-
+// bool contains(Object? element) {
+//   for (E e in this) {
+//     if (e == element) return true;
+//   }
+//   return false;
+//
