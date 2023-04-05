4a1a12ad5bac008a67ad41ea11f0ae5c627d13d4
“Dmitry” <shelllogist@gmail.com>
2023-04-05 23:50:49 +0300

    [List map]

--- -04_Lists/-04.22.01_additionalHomeWork.dart
+++ -04_Lists/-04.22.01_additionalHomeWork.dart
@@ -1,29 +1,39 @@
 void main() {
   // map: это метод, который позволяет применять функцию к каждому элементу
   // коллекции и создавать новую коллекцию на основе результата.
-  List<int> numbers = [1, 2, 3];
-  List<int> squares = numbers.map((numbers) => numbers * numbers).toList();
-  print(squares);
+  // List<int> numbers = [1, 2, 3];
+  // List<int> squares = numbers.map((number) => number * number).toList();
+  // print(squares);
+  // List<String> list1 = numbers.map((number) {
+  //   print(number);
+  //   return 'name $number';//.toString();
+  // }).toList();
+  // print(list1);
+  // List<String> listVegitables = ['tomato', 'carrot'];
+  // List<int> priceList = listVegitables.map((vegitable) =>
+  // vegitable == 'tomato' ? 2 : 4).toList();
+  // print(priceList);
 
-  //forEach: это метод, который позволяет выполнить функцию для каждого
+  // forEach: это метод, который позволяет выполнить функцию для каждого
   // элемента коллекции. Он не возвращает новую коллекцию.
   List<String> names = ['Alice', 'Bob', 'Charlie'];
   names.forEach((name) => print('Hello, $name!'));
 
   //join: это метод, который объединяет элементы коллекции в строку,
   // используя указанный разделитель.
-  List<String> words = ['hello', 'word'];
-  String greeting = words.join(' ');
-  print(greeting);
-
-  //fold и reduce: это методы, которые выполняют указанную операцию
-  // на элементах коллекции и возвращают результат. Разница между ними
-  // заключается в том, что fold принимает начальное значение для аккумулятора,
-  // а reduce использует первый элемент коллекции в качестве начального значения.
-  List<int> numbers = [1, 2, 3];
-  int sum1 = numbers.fold(0, (acc, number) => acc + number);
-  int sum2 = numbers.reduce((acc, number) => acc + number);
-  print(sum1);
+  // List<String> words = ['hello', 'word'];
+  // String greeting = words.join(' ');
+  // print(greeting);
+  //
+  // //fold и reduce: это методы, которые выполняют указанную операцию
+  // // на элементах коллекции и возвращают результат. Разница между ними
+  // // заключается в том, что fold принимает начальное значение для аккумулятора,
+  // // а reduce использует первый элемент коллекции в качестве начального значения.
+  // List<int> numbers1 = [1, 2, 3];
+  // int sum1 = numbers1.fold(2, (acc, number) => acc + number);
+  // int sum2 = numbers1.reduce((acc, number) => acc + number);
+  // print(sum1);
+  // print(sum2);
 
 
 }
