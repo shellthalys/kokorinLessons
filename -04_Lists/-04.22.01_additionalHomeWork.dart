87e9d748dc33e55dad3397d7769b36e144cd0516
“Dmitry” <shelllogist@gmail.com>
2023-04-07 00:06:07 +0300

    [List map, fold, add]

--- -04_Lists/-04.22.01_additionalHomeWork.dart
+++ -04_Lists/-04.22.01_additionalHomeWork.dart
@@ -35,5 +35,59 @@ void main() {
   // print(sum1);
   // print(sum2);
 
+  Map<String, dynamic> user1 = {
+    "name": "Denis",
+    "age": 16
+  };
 
+  Map<String, dynamic> user2 = {
+    "name": "Dmytro",
+    "age": 18
+  };
+
+  void main() {
+    final List<String> names = ["Petro", "Paraska"];
+
+    final List<Map<String, dynamic>> list = [user1, user2];
+
+    final List<String> mapedList = list.map((e) {
+      return e['name'].toString();
+    }).toList();
+
+    mapedList.add('Ivan');
+
+    mapedList.addAll(names);
+
+    mapedList.remove('Ivan');
+
+    final reducedList = mapedList.reduce((v, e) => v + e);
+
+    final foldList = [1,2,3,4,5];
+
+    final sum = foldList.fold(10, (previous, current) => previous + current);
+
+    final sortedList = mapedList.where((e) {
+      return e.toLowerCase().contains("d"); //if true
+    }).toList();
+
+    final singleElement = mapedList.singleWhere((e) => e == "Vasya", orElse: () => "Unknown");
+
+    final every = mapedList.every((e) => e == "Denis");
+
+    final singlePosition = mapedList.indexOf('Ivan');
+
+
+
+    print(mapedList.first);
+    print(mapedList.last);
+    print(mapedList.length);
+    print(mapedList.isEmpty);
+    print(mapedList.isNotEmpty);
+
+    final string = mapedList.join(":");
+    print(string);
+
+    final genereatedList = List.generate(10, (index) => "User $index");
+    print(genereatedList);
+  }
 }
