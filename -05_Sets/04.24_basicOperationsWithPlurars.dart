1dd98c6819ed068bd56c110d0df1f004b077d1b7
“Dmitry” <shelllogist@gmail.com>
2023-04-10 01:14:18 +0300

    [Set - creating listd,checking contents]

---
+++ -05_Sets/04.24_basicOperationsWithPlurars.dart
@@ -0,0 +1,17 @@
+void main() {
+  //creating set
+  final Set<int> setOne = {};
+  final setTwo = <int>{};
+  final setThree = <int>{1, 2, 3, 3};
+  print(setThree);
+  var setFour = Set.from([1, 'hello', true]);
+
+  //checking the contents
+  final fruits = {'apple', 'pineapple', 'watermelon'};
+  print(fruits.contains('apple'));
+  print(fruits.contains('pear'));
+
+  // adding and removing single elements
+
+  
+}
