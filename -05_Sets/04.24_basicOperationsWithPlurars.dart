8012e75ea37aa5da8ae5fb6e77097217855a87f9
“Dmitry” <shelllogist@gmail.com>
2023-04-11 00:58:15 +0300

    [Set - creating, add, remove, random]

--- -05_Sets/04.24_basicOperationsWithPlurars.dart
+++ -05_Sets/04.24_basicOperationsWithPlurars.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 void main() {
   //creating set
   final Set<int> setOne = {};
@@ -12,6 +14,50 @@ void main() {
   print(fruits.contains('pear'));
 
   // adding and removing single elements
-
+final newFruits = <String>{};
+  newFruits.add('orange');
+  newFruits.add('pear');
+  newFruits.add('orange');
+  print(newFruits);
+  newFruits.remove('pear');
+  print(newFruits);
   
+  //adding multiple elements
+  newFruits.addAll(['apple', 'pear', 'watermelon']);
+  print(newFruits);
+
+  //looping over the elements
+  for(final fruit in fruits) {
+    print('I\'m like $fruit');
+  }
+
+  //copying Sets
+  final freshFruits = fruits.toSet();
+  print(fruits);
+  freshFruits.remove('watermelon');
+  print(fruits);
+  print(freshFruits);
+
+  //other operations
+  var newFruits2 = {'pear', ...freshFruits, if (true) ...fruits};
+  print(newFruits2);
+
+  final randomGenerator = Random();
+  final randomList = <int>[];
+  for(int i = 0; i < 10; i++) {
+    final randomInt = randomGenerator.nextInt(10) + 1;
+    randomList.add(randomInt);
+  }
+  print(randomList);
+
+  final uniqueValue = <int>{};
+  final duplicates = <int>{};
+  for(int number in randomList) {
+    if(uniqueValue.contains(number)) {
+      duplicates.add(number);
+    }
+    uniqueValue.add(number);
+  }
+  print(duplicates);
+
 }
