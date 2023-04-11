8477463b9b0070058160ab8f1f4d4bc39d363494
shellthalys <shelllogist@gmail.com>
2023-04-11 21:12:51 +0300

    [list] - add -05.26

---
+++ -05_Sets/-05.26_exerciseFromBro.dart
@@ -0,0 +1,7 @@
+// що мені саме головне потрібно щоб ти знав
+// в нас є три типа Iterable (перечисляемые) List, Set, Map
+// final List<String> users = [“Ivan”, “Petro”];
+// final Set<String> users = {“Ivan”, “Petro”};
+// final Map<String, String> user = {“name”: “Ivan”};
+//
+// як витягнути з цих всих кейсів значення Ivan
