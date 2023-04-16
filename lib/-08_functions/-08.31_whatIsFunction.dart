dad4294d778bd1b1156d3e20390975c817dcc218
“Dmitry” <shelllogist@gmail.com>
2023-04-16 13:52:57 +0300

    [08_32 -> done]

--- lib/-08_functions/-08.31_whatIsFunction.dart
+++ lib/-08_functions/-08.31_whatIsFunction.dart
@@ -8,14 +8,17 @@ void main() {
   print(myAge);
 }
 
-void sayHello() {
-  print('Hello Dart!');
-}
+// void sayHello() {
+//   print('Hello Dart!');
+// }
+void sayHello() => print('Hello Dart');
 
-String getName() {
-  return 'Dart';
-}
+// String getName() {
+//   return 'Dart';
+// }
+String getName() => 'Dart';
 
-getAge(int age) {
-  return 'Your age $age';
-}
+// String getAge(int age) {
+//   return 'Your age $age';
+// }
+String getAge(int age) => 'Your age $age';
---
+++ lib/-08_functions/-08.32_parametersFunction.dart
@@ -0,0 +1,7 @@
+void main() {
+getPerson('Dart', 11);
+}
+
+void getPerson(String name, int age) {
+  print('Name: $name, Age: $age');
+}
