769930547e64875eef4ac48c894b0f5decb33537
“Dmitry” <shelllogist@gmail.com>
2023-04-20 01:06:37 +0300

    [08_34 -> start]

--- lib/-08_functions/-08.34_homeWorkFunction.dart
+++ lib/-08_functions/-08.34_homeWorkFunction.dart
@@ -2,17 +2,13 @@ void main() {
   final String alphabet = 'a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k'
     'l' 'm' 'n' 'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z';
   List<String> inputWords = ['abc', 'abc abc'];
-  String newAlphabet = alphabet.replaceAll(" ", "");
-  final indexAlphabet = newAlphabet.indexOf('z') + 1;
 
-
-
-  for(int i = 0; i < alphabet.length; i++) {
-    valueWord =
+  final List<int> mapedList = inputWords.map(() {
+    return e['name'].toString();
+  }).toList();
 
   }
 
-}
 
 
 
