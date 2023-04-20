5ca4729f351652f3d9327db1c83fea40823f4c9b
“Dmitry” <shelllogist@gmail.com>
2023-04-20 23:32:23 +0300

    [08_34 -> 1-st iterration]

--- lib/-08_functions/-08.34_homeWorkFunction.dart
+++ lib/-08_functions/-08.34_homeWorkFunction.dart
@@ -3,9 +3,18 @@ void main() {
     'l' 'm' 'n' 'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z';
   List<String> inputWords = ['abc', 'abc abc'];
 
-  final List<int> mapedList = inputWords.map(() {
-    return e['name'].toString();
+  final List<int> mapedList = inputWords.map((word) {
+    int sum = 0;
+    for(int i = 0; i < word.length; i++) {
+      String wordLetter = word[i];
+      if (alphabet.contains(wordLetter)) {
+        sum += alphabet.indexOf(wordLetter) + 1;
+      }
+    }
+    return sum * (inputWords.indexOf(word) + 1);
   }).toList();
+  
+  print(mapedList);
 
   }
 
