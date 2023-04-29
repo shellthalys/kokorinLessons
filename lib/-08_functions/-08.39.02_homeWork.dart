958c4d1aae7d24db914b978d82a957e2954af619
shellthalys <shelllogist@gmail.com>
2023-04-29 18:07:26 +0300

    add 08.39.02

--- lib/-08_functions/-08.39.02_homeWork.dart
+++ lib/-08_functions/-08.39.02_homeWork.dart
@@ -1,19 +1,23 @@
 void sumPositive() {
   List<int> inputWords1 = [1, -10, 9, -1];
-  List<int> inputWords2 = [-1, -2, -3];
-  List<int> inputWords3 = [];
-  List<int> inputWords4 = [1, 2];
+  // List<int> inputWords2 = [-1, -2, -3];
+  // List<int> inputWords3 = [];
+  // List<int> inputWords4 = [1, 2];
 
 
-  final int sumLenght1 = inputWords1.fold(0, (prev, curr) => prev + curr);
-  final int sumLenght2 = inputWords2.fold(0, (prev, curr) => prev + curr);
-  final int sumLenght3 = inputWords3.fold(0, (prev, curr) => prev + curr);
-  final int sumLenght4 = inputWords4.fold(0, (prev, curr) => prev + curr);
+  final int sumLenght1 = inputWords1.fold(0, (prev, curr){
+    print(prev);
+    print(curr);
+    return prev + curr;
+  });
+  // final int sumLenght2 = inputWords2.fold(0, (prev, curr) => prev + curr);
+  // final int sumLenght3 = inputWords3.fold(0, (prev, curr) => prev + curr);
+  // final int sumLenght4 = inputWords4.fold(0, (prev, curr) => prev + curr);
 
 
   print(sumLenght1);
-  print(sumLenght2);
-  print(sumLenght3);
-  print(sumLenght4);
+  // print(sumLenght2);
+  // print(sumLenght3);
+  // print(sumLenght4);
 
 }
