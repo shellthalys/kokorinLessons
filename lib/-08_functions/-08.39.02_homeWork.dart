404fc37749acb86a4063f635d9ffb0da68f6bb99
shellthalys <shelllogist@gmail.com>
2023-04-30 18:54:09 +0300

    [08.39.02] -> done with 4 methods

--- lib/-08_functions/-08.39.02_homeWork.dart
+++ lib/-08_functions/-08.39.02_homeWork.dart
@@ -1,23 +1,39 @@
+// рассмотрены несколько подходов в решении задачи
+
 void sumPositive() {
-  List<int> inputWords1 = [1, -10, 9, -1];
-  // List<int> inputWords2 = [-1, -2, -3];
-  // List<int> inputWords3 = [];
-  // List<int> inputWords4 = [1, 2];
+  List<int> inputWords1 = [5, -10, 9, -1];
+  List<int> inputWords2 = [-1, -2, -3];
+  List<int> inputWords3 = [1, 2, -5];
+  List<int> inputWords4 = [1, 2];
 
 
-  final int sumLenght1 = inputWords1.fold(0, (prev, curr){
-    print(prev);
-    print(curr);
-    return prev + curr;
-  });
-  // final int sumLenght2 = inputWords2.fold(0, (prev, curr) => prev + curr);
-  // final int sumLenght3 = inputWords3.fold(0, (prev, curr) => prev + curr);
-  // final int sumLenght4 = inputWords4.fold(0, (prev, curr) => prev + curr);
+  // final int sumLenght1 = inputWords1.where((int num) {
+  //   return num > 0;
+  // }).fold(0, (prev, curr) {
+  //   // print('Попереднє значення $prev та поточне $curr');
+  //   return prev + curr;
+  // });
 
+  // final int sumLenght2 = inputWords2.fold(0, (prev, curr) {
+  //   if (curr > 0) {
+  //     print('Попереднє значення $prev та поточне $curr');
+  //     return prev + curr;
+  //   } else {
+  //     return 0;
+  //   }
+  // });
+  // final int sumLenght3 = inputWords3.fold(0, (prev, curr) {
+  //   if(curr.isNegative) {
+  //     return prev;
+  //   } else {
+  //     return prev + curr;
+  //   }
+  // });
+  final int sumLenght4 = inputWords4.fold(0, (prev, curr) => !curr.isNegative ? prev + curr : prev);
 
-  print(sumLenght1);
+  // print(sumLenght1);
   // print(sumLenght2);
   // print(sumLenght3);
-  // print(sumLenght4);
+  print(sumLenght4);
 
 }
