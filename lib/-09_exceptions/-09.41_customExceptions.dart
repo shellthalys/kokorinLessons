7308f007a2fa3870b01c1cda4a0f434294dc87c2
“Dmitry” <shelllogist@gmail.com>
2023-05-02 01:23:42 +0300

    [09_41 -> start do it]

---
+++ lib/-09_exceptions/-09.41_customExceptions.dart
@@ -0,0 +1,28 @@
+void main() {
+  try {
+    print(factorial(-5));
+  } catch (e) {
+    if(e is FactorialException) {
+      print(e.errorMessage());
+    } else {
+      print('Unexpected error $e');
+    }
+  } finally {
+    //Code
+  }
+}
+
+class FactorialException implements Exception {
+  String errorMessage() {
+    return 'Input number must be more than zero';
+  }
+}
+
+int factorial(int n) {
+  if(n < 1) throw FactorialException();
+  int fact = 1;
+  for(int i = 2; i <= n; i++) {
+    fact *= i;
+  }
+  return fact;
+}
