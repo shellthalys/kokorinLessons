b81cc3cf3ab17a47816863efa5d264a918d9082b
“Dmitry” <shelllogist@gmail.com>
2023-05-02 19:28:38 +0300

    [10.42 -> start do it]

--- lib/kokorin.dart
+++ lib/kokorin.dart
@@ -1,6 +1,12 @@
 // TODO Implement this library.
-import 'package:kokorin/-08_functions/-08.39.02_homeWork.dart' as lib;
+// import 'package:kokorin/-08_functions/-08.39.02_homeWork.dart' as lib;
+//
+// void main() {
+//   lib.sumPositive();
+//   }
+
+import 'package:kokorin/-09_exceptions/-09.41_customExceptions.dart' as lib;
 
 void main() {
-  lib.sumPositive();
-  }
+  lib.factorExcept();
+}
