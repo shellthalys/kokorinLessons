d7ccaa499b790b81ff6f37a7fa574ec12c638a47
“Dmitry” <shelllogist@gmail.com>
2023-05-13 00:34:59 +0300

    [10.51 -> done it]

--- lib/-11_oop2/-11.51_inheritance.dart
+++ lib/-11_oop2/-11.51_inheritance.dart
@@ -1,25 +1,36 @@
 void carClass() {
 
+  var myCar = Car();
+  myCar.carName = 'Mercedes';
+  myCar.color = 'black';
+  myCar.open();
+  myCar.start();
+
+var myMoto = Moto();
+myMoto.type = 'Sportbike';
+myMoto.color = 'grey';
+myMoto.roll();
+myMoto.start();
 }
 
 class Vechicle {
-  String color;
+  late String color;
 
   void start() {
     print('Has started');
   }
 }
 
-class Car {
-  String carName;
+class Car extends Vechicle {
+  late String carName;
   
   void open() {
     print('Open doors');
   }
 }
 
-class Moto {
-  String type;
+class Moto extends Vechicle {
+  late String type;
 
   void roll() {
     print('Rolled fast run');
--- lib/kokorin.dart
+++ lib/kokorin.dart
@@ -1,12 +1,12 @@
 // TODO Implement this library.
-// import 'package:kokorin/-08_functions/-08.39.02_homeWork.dart' as lib;
-//
-// void main() {
-//   lib.sumPositive();
-//   }
-
-import 'package:kokorin/-11_oop2/-11.51_inheritance.dart as lib;
+import 'package:kokorin/-11_oop2/-11.51_inheritance.dart' as lib;
 
 void main() {
   lib.carClass();
-}
+  }
+
+// import 'package:kokorin/-11_oop2/-11.51_inheritance.dart as lib;
+//
+// void main() {
+//   lib.carClass();
+// }
