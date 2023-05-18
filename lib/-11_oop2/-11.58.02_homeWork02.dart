6f693ae652a18bab2b610b8d2b0b994065fe56cb
“Dmitry” <shelllogist@gmail.com>
2023-05-18 22:23:28 +0300

    [10.58.02 -> done it]

--- lib/-11_oop2/-11.58.02_homeWork02.dart
+++ lib/-11_oop2/-11.58.02_homeWork02.dart
@@ -4,14 +4,21 @@ Cuboid cuboid = Cuboid(1, 2, 3);
 int surfaceAreaResult = cuboid.surfaceArea;
 int volumeResult = cuboid.volume;
 
+Cube cube = Cube(2);
+int cubeSurfaceAreaResult = cube.cubeSurfaceArea;
+int cubeVolumeResult = cube.cubeVolume;
+
 print('Result surfaceArea: $surfaceAreaResult');
 print('Result volume: $volumeResult');
+
+print('Result cubeSurfaceArea: $cubeSurfaceAreaResult');
+print('Result cubeVolume: $cubeVolumeResult');
 }
 
 class Cuboid {
-  int length;
-  int width;
-  int height;
+  final int length;
+  final int width;
+  final int height;
 
   Cuboid(this.length, this.width, this.height);
 
@@ -22,4 +29,20 @@ int get surfaceArea {
 int get volume {
     return length * width * height;
   }
+}
+
+class Cube extends Cuboid {
+  final int length;
+
+  Cube(this.length): super(length, length, length);
+
+  @override
+  int get cubeSurfaceArea {
+    return 6 * (length * length);
+  }
+
+  int get cubeVolume {
+    return length * length * length;
+  }
+
 }
--- lib/kokorin.dart
+++ lib/kokorin.dart
@@ -1,11 +1,11 @@
 // TODO Implement this library.
-import 'package:kokorin/-11_oop2/-11.58.01_homeWork01.dart' as lib;
+import 'package:kokorin/-11_oop2/-11.58.02_homeWork02.dart' as lib;
 
 void main() {
   lib.vehicleClass();
   }
 
-// import 'package:kokorin/-11_oop2/-11.51_inheritance.dart as lib;
+// import 'package:kokorin/-11_oop2/-11.58.02_homeWork02.dart' as lib;
 //
 // void main() {
 //   lib.carClass();
